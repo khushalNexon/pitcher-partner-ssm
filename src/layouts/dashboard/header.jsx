f98cf7d9c84736946782927ac682d707cce2d6d2
khushalbatham22 <52168782+khushalbatham22@users.noreply.github.com>
2024-07-08 19:25:29 +0530

    bug: remove hamburger

--- src/layouts/dashboard/header.jsx
+++ src/layouts/dashboard/header.jsx
@@ -5,13 +5,13 @@ import Stack from '@mui/material/Stack';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import { useTheme } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
+// import IconButton from '@mui/material/IconButton';
 
 import { useResponsive } from 'src/hooks/use-responsive';
 
 import { bgBlur } from 'src/theme/css';
 
-import Iconify from 'src/components/iconify';
+// import Iconify from 'src/components/iconify';
 
 import { NAV, HEADER } from './config-layout';
 import AccountPopover from './common/account-popover';
@@ -25,16 +25,19 @@ export default function Header({ onOpenNav }) {
 
   const renderContent = (
     <>
-      {!lgUp && (
+      {/* {!lgUp && (
         <IconButton onClick={onOpenNav} sx={{ mr: 1 }}>
           <Iconify icon="eva:menu-2-fill" />
         </IconButton>
-      )}
-
+      )} */}
 
       <Box sx={{ flexGrow: 1 }} />
 
-      <Stack direction="row" alignItems="center" spacing={1}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={1}
+      >
         <AccountPopover />
       </Stack>
     </>
